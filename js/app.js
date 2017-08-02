520cec650ac67fcbfdf452ed4cf92593f1531092
fbarriosr <francisco.barriosr@gmail.com>
2017-08-02 08:44:28 -0400

    vesion1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).foundation()
+/*
+ document.querySelector('#btn2').onclick = function(){
+	    swal("Dowload!", "Ver el proceso en Ipad Home", "success");
+	    swal.setDefaults({ confirmButtonColor: 'green' });
+	    };
+	    */
+
